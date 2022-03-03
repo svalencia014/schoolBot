76f4d8cd8138f132890354e62ce07771bc284ec7
Samuel Valencia <sv.samuel.valencia014@gmail.com>
2022-03-03 11:11:35 -0500

    update

--- commands/verify.js
+++ commands/verify.js
@@ -4,6 +4,7 @@ module.exports = {
     name: 'verify',
     description: 'Allows the User to verify',
     execute(message, args) {
-        message.author.send("test");
+        message.author.send("Thank you for joining the {discord name} server!");
+        message.author.send("We need to verify a few things before we give you access.");
     }
 }
