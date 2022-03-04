39099d7ab97f434fb4e5b918a5a06c0484c9b1de
root <root@DESKTOP-KIFJ7Q8.localdomain>
2022-03-04 08:09:44 -0500

    update 4

--- commands/verify.js
+++ commands/verify.js
@@ -1,4 +1,6 @@
 const { Client, Intents } = require('discord.js');
+items = 1;
+email = null;
 function sleep(ms) {
     return new Promise(resolve => setTimeout(resolve, ms));
 }
@@ -7,18 +9,22 @@ module.exports = {
     name: 'verify',
     description: 'Allows the User to verify',
     execute(message, args) {
+        let memberTarget = message.guild.members.cache.get(message.author.id);
         message.channel.send("We need to verify a few things before we give you access.");
         message.channel.send("What is your **school** email address?");
         sleep(5500);
         const filter = (message) => !message.author.bot
-        const collector = message.channel.createMessageCollector({ filter, time: 5000 });
+        const collector = message.channel.createMessageCollector({ filter, max: 1, time: 5000 });
         collector.on('collect', m => {
             console.log(`Collected ${m.content}`);
         });
 
         collector.on('end', collected => {
             console.log(`Collected ${collected.size} items`);
+            email = collected;
         });
+        
+
         var code = Math.floor(100000 + Math.random() * 900000)
         console.log(code);
         
