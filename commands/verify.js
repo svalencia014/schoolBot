82f316cd43cd84700c8c08ea3e9242db3128b8c5
Samuel Valencia <sv.samuel.valencia014@gmail.com>
2022-03-07 17:19:08 -0500

    Setup workflows

--- commands/verify.js
+++ commands/verify.js
@@ -13,24 +13,28 @@ module.exports = {
     name: 'verify', 
     description: 'Allows the User to verify',
     execute(message, args) {
-        
-        let memberTarget = message.guild.members.cache.get(message.author.id);
-        message.channel.send("We need to verify a few things before we give you access.");
-        message.channel.send("What is your **school** email address?");
-        sleep(5500);
-        const filter = (message) => !message.author.bot || message.author == memberTarget;
-        const collector = message.channel.createMessageCollector({ filter, max: 1, time: 10000 });
-        collector.on('collect', m => {
-            console.log(`Collected ${m.content}`);
-        });
+        async function getEmail(email) {     
+            let memberTarget = message.guild.members.cache.get(message.author.id);
+            message.channel.send("We need to verify a few things before we give you access.");
+            message.channel.send("What is your **school** email address?");
+            sleep(5500);
+            const filter = (message) => !message.author.bot || message.author == memberTarget;
+            const collector = message.channel.createMessageCollector({ filter, max: 1, time: 10000 });
+            collector.on('collect', m => {
+                console.log(`Collected ${m.content}`);
+            });
 
-        collector.on('end', collected => {
-            console.log(`Collected ${collected.size} items`);
-            email = collected.first();
-            console.log(email.content);
-            userEmail = email.content;              //The email of the recipient
-            message.channel.send("Thank you. Please wait a moment while I email you a code.");
-        });
+            collector.on('end', collected => {
+                console.log(`Collected ${collected.size} items`);
+                email = collected.first();
+                console.log(email.content);
+                userEmail = email.content;              //The email of the recipient
+                message.channel.send("Thank you. Please wait a moment while I email you a code.");
+            });
+        }
+        while (email == null) {
+            getEmail();
+        }
 
         var code = Math.floor(100000 + Math.random() * 900000)
         console.log(code);
